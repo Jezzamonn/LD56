208509c2ce0c80009778f418e019574bcdd13c3d
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2024-10-05 23:34:22 -0700

    Show totals of different guy types

--- src/ts/game/ui/guy-totals.ts
+++ src/ts/game/ui/guy-totals.ts
@@ -1,9 +1,28 @@
-import { Guy } from "../entity/guy";
+import { Guy, GuyType } from "../entity/guy";
 
-const guyTotalsElem = document.querySelector('.guy-totals')!;
+const types: GuyType[] = [GuyType.Normal, GuyType.Fire];
 
 export namespace GuyTotals {
     export function updateGuyTotals(availableGuys: Guy[], knownGuys: Guy[]): void {
-        guyTotalsElem.innerHTML = `${availableGuys.length}/${knownGuys.length}`;
+        const availableTotals = new Map<GuyType, number>();
+        const knownTotals = new Map<GuyType, number>();
+        for (const type of types) {
+            availableTotals.set(type, 0);
+            knownTotals.set(type, 0);
+        }
+
+        for (const guy of availableGuys) {
+            const type = availableTotals.has(guy.type) ? guy.type : GuyType.Normal;
+            availableTotals.set(type, availableTotals.get(type)! + 1);
+        }
+        for (const guy of knownGuys) {
+            const type = knownTotals.has(guy.type) ? guy.type : GuyType.Normal;
+            knownTotals.set(type, knownTotals.get(type)! + 1);
+        }
+
+        for (const type of types) {
+            const elem = document.querySelector(`.guy-total-${type}`)! as HTMLElement;
+            elem.innerText = `${availableTotals.get(type)!}/${knownTotals.get(type)!}`;
+        }
     }
 }
